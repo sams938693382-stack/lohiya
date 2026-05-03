9da0597e5e05acd4a298a0e10d702acd729793eb
XURSANDBEK <sams938693382@gmail.com>
2026-05-03 17:07:20 +0500

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,78 @@
+# muhammadjonn = {
+#     'ism':'muhammadjonn',
+#     'yosh':22,
+#     'yolgonchi':False,
+#     't__yili':2010,
+#     'tel_raqam':'+998777645123',
+#     'manzil':'qora qosh'
+# }
+# print(f"{muhammadjonn} ning malumot tipi {type(muhammadjonn)}")
+
+# print(muhammadjonn['ism'].title())
+# print(muhammadjonn['tel_raqam'])
+# print(f"{muhammadjonn['ism'].title() }ning tel nomeri {muhammadjonn['tel_raqam']}")
+
+
+
+# sonlar = ()
+# qiymat1 = int(input("istalgan butun sonni kiriting"))
+# qiymat2 = int(input("istalgan qiymatni kiriting"))
+# kopaytrish = (qiymat1**qiymat2)
+# print(kopaytrish)
+
+
+
+# qiymat1 = int(input("istalgan butun sonni kiriting"))
+# qiymat2 = int(input("istalgan qiymatni kiriting"))
+# qiymat3 =int(input("istalgan sonlarni kiritin"))
+# bolish = (qiymat1/qiymat2/qiymat3)
+# print(bolish)
+
+# moshinalar = {
+#     'nomi':'malibu',
+#     'nomeri':'555',
+#     'shinasi':'michelen',
+#     'motori':'1.8',
+#     'chiqan yili':'2022'
+
+# }
+# print(f"{moshinalar} ning malumot tipi {type(moshinalar)}")
+
+# moshinalar = {
+#     'cobalt': ['1.6','oq rang','mexanik','2020'],
+#     'onix': ('1.nicha','oq rang','avtomot','2025'),
+#     'malibu': ['turbo','oq rang','mexanik','2020']
+# }
+
+# print(moshinalar['onix'])
+
+ 
+ichki_lugatlar = {
+    'ozbekiston': {
+        'toshkent' : '01 va 10 noshina nomeri',
+        'xorazm': '90 moshina nomeri',
+        'qashqadaryo': '70 moshina nomeri',
+        'surxandaryo': '75 moshina nomeri'
+    },
+    'china motors': {
+        'BYD': 'yotong',
+        'changan': 'depal s 07',
+        'ziker': '001'
+    },
+    'germany motors':{
+        'BMW': 'm5',
+        'merceders': 'cls 63',
+        'audi' : 'i3',
+        'porsche':'911',
+    },
+    'uz avto' : {
+        'chevralet':'malubu',
+        'daewoo' : 'tico',
+        'ravon' : 'nexia 3'
+    }
+}
+
+print(ichki_lugatlar['ozbekiston'])
+
+
+
